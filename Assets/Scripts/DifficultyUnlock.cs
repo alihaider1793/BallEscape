6b5f77c1967fd219626405a8321f0db29011eade
Ali Haider <alihaider1793@gmail.com>
2018-04-07 20:19:20 +0500

    done
    deploying

--- Assets/Scripts/DifficultyUnlock.cs
+++ Assets/Scripts/DifficultyUnlock.cs
@@ -7,7 +7,7 @@ using GoogleMobileAds.Api;
 public class DifficultyUnlock : MonoBehaviour {
 
     public GameObject easyBtn, mediumBtn, hardBtn;
-
+    public Sprite lockImg,unlockImg;
 	// Use this for initialization
 	void Start () {
         
@@ -15,22 +15,40 @@ public class DifficultyUnlock : MonoBehaviour {
         if (PlayerPrefs.GetInt("levelCleard") < 4)
         {
             easyBtn.GetComponent<Button>().interactable = true;
+
             mediumBtn.GetComponent<Button>().interactable = false;
+            mediumBtn.GetComponentInChildren<Text>().text = "";
+            mediumBtn.GetComponent<Button>().image.sprite = lockImg;
+
             hardBtn.GetComponent<Button>().interactable = false;
+            hardBtn.GetComponentInChildren<Text>().text = "";
+            hardBtn.GetComponent<Button>().image.sprite = lockImg;
         }
 
         if (PlayerPrefs.GetInt("levelCleard") >= 5)
         {
             easyBtn.GetComponent<Button>().interactable = true;
+
             mediumBtn.GetComponent<Button>().interactable = true;
+            mediumBtn.GetComponentInChildren<Text>().text = "MEDIUM";
+            mediumBtn.GetComponent<Button>().image.sprite = unlockImg;
+
             hardBtn.GetComponent<Button>().interactable = false;
+            hardBtn.GetComponentInChildren<Text>().text = "";
+            hardBtn.GetComponent<Button>().image.sprite = lockImg;
         }
            
         if (PlayerPrefs.GetInt("levelCleard") >9)
         {
-            mediumBtn.GetComponent<Button>().interactable = true;
-            hardBtn.GetComponent<Button>().interactable = true;
             easyBtn.GetComponent<Button>().interactable = true;
+
+            mediumBtn.GetComponent<Button>().interactable = true;
+            mediumBtn.GetComponentInChildren<Text>().text = "MEDIUM";
+            mediumBtn.GetComponent<Button>().image.sprite = unlockImg;
+
+            hardBtn.GetComponent<Button>().interactable = true;
+            hardBtn.GetComponent<Button>().image.sprite = unlockImg;
+            hardBtn.GetComponentInChildren<Text>().text = "HARD";
         }
     }
 	
